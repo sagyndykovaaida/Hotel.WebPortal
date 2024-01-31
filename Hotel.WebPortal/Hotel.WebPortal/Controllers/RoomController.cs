c99440855fea2530be20323ae3c72bf66c19b4b2
sagyndykovaaida <alievnasg@gmail.com>
2024-01-31 14:15:32 +0600

    create new project mvc, created room views and controller

---
+++ Hotel.WebPortal/Hotel.WebPortal/Controllers/RoomController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.WebPortal.Controllers
+{
+    public class RoomController : Controller
+    {
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View();
+        }
+
+
+    }
+}
